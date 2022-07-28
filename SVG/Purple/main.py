932d12686b0618397c4be4ec133affeadf6fcb46
alvelazquez <alvelazquez@creditoreal.com.mx>
2022-07-28 15:36:49 -0500

    rename 134 files and update 1 file

--- SVG/Purple/main.py
+++ SVG/Purple/main.py
@@ -4,4 +4,8 @@ files = os.listdir(path)
 
 
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, file.replace('_black')))
+    print(file)
+    src = os.path.join(path, file)
+    dest = os.path.join(path, file.replace('_black',''))
+    print(src,dest)
+    os.rename(src, dest)
