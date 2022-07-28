28add0d138b2fc670ffadac54817c629fbd6f07f
alvelazquez <alvelazquez@creditoreal.com.mx>
2022-07-28 15:33:50 -0500

    update main.py

--- SVG/Purple/main.py
+++ SVG/Purple/main.py
@@ -4,4 +4,4 @@ files = os.listdir(path)
 
 
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
+    os.rename(os.path.join(path, file), os.path.join(path, file.replace('_black')))
